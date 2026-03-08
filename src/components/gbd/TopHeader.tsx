a1674cd0d8d09e4edf8da9ec9578708700726838
Xeesan <ziauddinzisan2001@gmail.com>
2026-03-08 12:25:22 +0000

    Preceding changes

--- src/components/gbd/TopHeader.tsx
+++ src/components/gbd/TopHeader.tsx
@@ -18,7 +18,7 @@ const TopHeader = ({ onToggleSidebar, onNavigate, calendarOpen, onToggleCalendar
 
   return (
     <header className="grid grid-cols-3 items-center px-3 sm:px-6 h-[56px] sm:h-[60px] min-h-[56px] sm:min-h-[60px] relative" style={{ borderBottom: '1px solid hsl(var(--border))' }}>
-      <button className="hamburger-btn" onClick={onToggleSidebar}>
+      <button className="hamburger-btn" onClick={onToggleSidebar} aria-label="Toggle sidebar menu">
         <Menu className="w-5 h-5" />
       </button>
       <button
