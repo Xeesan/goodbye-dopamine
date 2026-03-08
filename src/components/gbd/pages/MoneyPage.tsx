80664dbe72b4928f92eb6a62db6e4f62be75d8ff
Xeesan <ziauddinzisan2001@gmail.com>
2026-03-08 11:04:19 +0000

    Changes

--- src/components/gbd/pages/MoneyPage.tsx
+++ src/components/gbd/pages/MoneyPage.tsx
@@ -3,6 +3,7 @@ import Storage from '@/lib/storage';
 import { formatDate } from '@/lib/helpers';
 import { ArrowLeft } from 'lucide-react';
 import { useDialog } from '../DialogProvider';
+import { toast } from '@/hooks/use-toast';
 import { useGamification } from '@/hooks/useGamification';
 
 interface MoneyPageProps {
