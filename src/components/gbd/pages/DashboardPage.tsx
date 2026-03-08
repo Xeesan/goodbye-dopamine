bb0059904cc68384e615a6f3efc184734ba685be
Xeesan <ziauddinzisan2001@gmail.com>
2026-03-08 10:49:40 +0000

    Add MarkdownRenderer and Calendar widget

--- src/components/gbd/pages/DashboardPage.tsx
+++ src/components/gbd/pages/DashboardPage.tsx
@@ -3,6 +3,7 @@ import Storage from '@/lib/storage';
 import { getDailyQuote, getRandomQuote } from '@/lib/quotes';
 import { useGamification } from '@/hooks/useGamification';
 import { Clock, CheckSquare, BarChart3, Heart, Zap, Star, RefreshCw, Link, Settings, Calendar, Monitor, Wallet, StickyNote, BookOpen, Timer, FileText } from 'lucide-react';
+import UnifiedCalendarWidget from '../UnifiedCalendarWidget';
 
 interface DashboardPageProps {
   navigateTo: (page: string) => void;
